95ce3c8d758466de92e03d45c7e6252e432910c1
Kayla <whatsup@kayladgriffin.com>
2021-08-09 15:56:19 -0500

    updates

--- realadvantage/js/previews/front_page_areas-preview.php
+++ realadvantage/js/previews/front_page_areas-preview.php
@@ -1,5 +1,5 @@
 <?php //front_page_areas for builder ?>
 
-<script type="text/template" id="fusion-builder-block-module-front_page_reviews-preview-template">
+<script type="text/template" id="fusion-builder-block-module-front_page_areas-preview-template">
 	<h4 class="fusion_module_title"><span class="fusion-module-icon {{ fusionAllElements[element_type].icon }}"></span>Front Page Areas</h4>
 </script>
