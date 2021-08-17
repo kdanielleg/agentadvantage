903b63d60aa9cd94347f68699e48009c42bd37e0
Kayla <whatsup@kayladgriffin.com>
2021-08-17 18:03:30 -0500

    updates

--- realadvantage/js/previews/template_reviews-preview.php
+++ realadvantage/js/previews/template_reviews-preview.php
@@ -1,5 +1,5 @@
-<?php //page_reviews for builder ?>
+<?php //template_reviews for builder ?>
 
-<script type="text/template" id="fusion-builder-block-module-page_reviews-preview-template">
+<script type="text/template" id="fusion-builder-block-module-template_reviews-preview-template">
 	<h4 class="fusion_module_title"><span class="fusion-module-icon {{ fusionAllElements[element_type].icon }}"></span>Reviews - Admin Template Use Only</h4>
 </script>
