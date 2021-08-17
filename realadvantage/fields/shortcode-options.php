d35a73d186a10fd83500cb003517e9367bdb844a
Kayla <whatsup@kayladgriffin.com>
2021-08-17 14:51:08 -0500

    updates

--- realadvantage/fields/shortcode-options.php
+++ realadvantage/fields/shortcode-options.php
@@ -31,81 +31,11 @@ if( function_exists('acf_add_local_field_group') ):
 				array(
 					'param' => 'post_type',
 					'operator' => '==',
-					'value' => 'avada_portfolio',
-				),
-			),
-			array(
-				array(
-					'param' => 'post_type',
-					'operator' => '==',
 					'value' => 'post',
-				),
-			),
-			array(
-				array(
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'page',
 				),
 			),
 		),
 		'menu_order' => 0,
-		'position' => 'side',
-		'style' => 'default',
-		'label_placement' => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen' => '',
-		'active' => true,
-		'description' => '',
-	));
-	//location map
-	acf_add_local_field_group(array(
-		'key' => 'group_5cda671125412',
-		'title' => 'Walkscore and Schools',
-		'fields' => array(
-			array(
-				'key' => 'field_5cda671d0a859',
-				'label' => 'Map Location',
-				'name' => 'location',
-				'type' => 'google_map',
-				'instructions' => 'This field is REQUIRED to set the location for the walkscore or schools shortcodes.',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'center_lat' => '',
-				'center_lng' => '',
-				'zoom' => '',
-				'height' => '150',
-			),
-		),
-		'location' => array(
-			array(
-				array(
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'post',
-				),
-			),
-			array(
-				array(
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'avada_portfolio',
-				),
-			),
-			array(
-				array(
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'page',
-				),
-			),
-		),
-		'menu_order' => 1,
 		'position' => 'side',
 		'style' => 'default',
 		'label_placement' => 'top',
