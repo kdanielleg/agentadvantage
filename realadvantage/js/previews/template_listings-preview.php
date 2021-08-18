d2d179bec0a07552deb0192e90fa5578d894263a
Kayla <whatsup@kayladgriffin.com>
2021-08-17 22:03:49 -0500

    updates

---
+++ realadvantage/js/previews/template_listings-preview.php
@@ -0,0 +1,5 @@
+<?php //template_listings for builder ?>
+
+<script type="text/template" id="fusion-builder-block-module-template_listings-preview-template">
+	<h4 class="fusion_module_title"><span class="fusion-module-icon {{ fusionAllElements[element_type].icon }}"></span>Listings - Admin Template Use Only</h4>
+</script>
