cd7684961e8401deec185a09887503d6fea21e44
Kayla <whatsup@kayladgriffin.com>
2021-08-24 11:25:42 -0500

    updates

--- realadvantage/idx/themes/theme2020/js/roster_js.php
+++ realadvantage/idx/themes/theme2020/js/roster_js.php
@@ -39,47 +39,17 @@ endif;
 	});
 	$('div#ar-idx-disclaimer').append($('div#IDX-main > div:last-child'));
 	$('hr#IDX-officeBreak').remove();
-<?php if(get_field('ar_client_file','option')=='whitediamond'): ?>
 	$('div#IDX-rosterFilterForm').remove();
 	$('.IDX-rosterAgentWrap').removeClass('col-sm-6 col-md-3 IDX-roster-category').wrap('<div class="arAgentOuterWrap col-sm-12 col-md-6 col-lg-3">');
 	$('.IDX-rosterStaffWrap').removeClass('col-sm-6 col-md-3 IDX-roster-category').wrap('<div class="arStaffOuterWrap col-sm-12 col-md-6 col-lg-3">');
-	$('.IDX-rosterCategoryTitle').removeClass('col-xs-12');
-	$('.IDX-rosterCategoryTitle').each(function(){
-		$(this).before('<h2 class="'+$(this).attr('class')+'">'+$(this).text().trim()+'</h2>');
-		$(this).remove();
-	});
-	$('h2.IDX-rosterStaffTitle').text('Support Staff');
-	$('h2.IDX-rosterAgentTitle').text('Our Agents');
-	$('div#IDX-main > .IDX-rosterAgentContent').wrap('<div id="wdrAgentsWrap"><div class="IDX-rosterSectionContentWrap">');
-	$('div#IDX-main > .IDX-rosterStaffContent').wrap('<div id="wdrStaffWrap"><div class="IDX-rosterSectionContentWrap">');
-	$('#wdrAgentsWrap').prepend('<div class="fusion-menu-anchor anchorFix" id="wdrAgents"></div>');
-	$('#wdrStaffWrap').prepend('<div class="fusion-menu-anchor anchorFix" id="wdrStaff"></div>');
-<?php elseif(get_field('ar_client_file','option')=='wpirealestate'): ?>
-	$('form#IDX-rosterFilter select#IDX-sort > option:first-child').text('Sort By...');
-	$('.IDX-rosterAgentWrap').removeClass('col-sm-6 col-md-3 IDX-roster-category').wrap('<div class="arAgentOuterWrap col-sm-12 col-md-6 col-lg-3">');
-	$('.IDX-rosterStaffWrap').removeClass('col-sm-6 col-md-3 IDX-roster-category').wrap('<div class="arStaffOuterWrap col-sm-12 col-md-6 col-lg-3">');
-	$('.IDX-rosterCategoryTitle').removeClass('col-xs-12');
-	$('.IDX-rosterCategoryTitle').each(function(){
-		$(this).before('<h2 class="'+$(this).attr('class')+'">'+$(this).text().trim()+'</h2>');
-		$(this).remove();
-	});
-	$('h2.IDX-rosterStaffTitle').text('Property Management & Staff').wrap('<div id="rosterStaffSortWrap" class="rosterSortHeaderWrap IDX-row"><div id="rosterStaffSortTitle" class="col-sm-12 col-md-7">');
-	$('#rosterStaffSortWrap').append('<div class="col-sm-12 col-md-5" id="rosterStaffSort"></div>');
-	$('#rosterStaffSort').append($('div#IDX-rosterFilterForm'));
-	$('h2.IDX-rosterAgentTitle').text('Real Estate Brokers').wrap('<div id="rosterAgentSortWrap" class="rosterSortHeaderWrap IDX-row"><div class="col-sm-12 col-md-7" id="rosterAgentSortTitle">');
-	$('#rosterAgentSortWrap').append('<div class="col-sm-12 col-md-5" id="rosterAgentSort"></div>');
-	$('#rosterAgentSort').append($('div#IDX-rosterFilterForm').clone());
-	$('div#IDX-main > .IDX-rosterAgentContent').wrap('<div id="wpiAgentsWrap"><div class="IDX-rosterSectionContentWrap">');
-	$('div#IDX-main > .IDX-rosterStaffContent').wrap('<div id="wpiStaffWrap"><div class="IDX-rosterSectionContentWrap">');
-<?php else: ?>
-	$('div#IDX-rosterFilterForm').remove();
-	$('.IDX-rosterAgentWrap').removeClass('col-sm-6 col-md-3 IDX-roster-category').wrap('<div class="arAgentOuterWrap col-sm-12 col-md-6 col-lg-3">');
-	$('.IDX-rosterStaffWrap').removeClass('col-sm-6 col-md-3 IDX-roster-category').wrap('<div class="arStaffOuterWrap col-sm-12 col-md-6 col-lg-3">');
-	$('.IDX-rosterCategoryTitle').remove();	
-<?php endif; ?>
+	//$('.IDX-rosterCategoryTitle').remove();	
+
 
 	$('.IDX-rosterAgentImageWrap, .IDX-rosterStaffImageWrap').removeClass('col-xs-12').unwrap().unwrap().unwrap();
 	$('.IDX-rosterAgentInfoWrap, .IDX-rosterStaffInfoWrap').removeClass('col-xs-12 IDX-rosterCategoryRightColumn');
+
+	$('.IDX-row').addClass('row');
+
 <?php if(!$showFields || $showOnlyTitle): ?>
 	$('.IDX-rosterInfo').remove();
 	$('address#IDX-rosterAddress').remove();
@@ -113,7 +83,7 @@ endif;
 	});
 	$('.IDX-rosterInnerWrap + .IDX-row').unwrap().remove();
 <?php endif; ?>
-	$('.IDX-row').addClass('row');
+	
 <?php if(get_field('ar_client_file','option')=='whitediamond'): ?>
 	$('.arAgentOuterWrap').wrapAll('<div class="IDX-row agents-row row">');
 	$('.arStaffOuterWrap').wrapAll('<div class="IDX-row staff-row row">');
